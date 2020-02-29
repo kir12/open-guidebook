1396763254588ac11623b857a1883bbfc5530d32
Brian Lee <blee025432@gmail.com>
2020-02-29 13:08:44 -0500

    added more serializers

---
+++ schedule/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Event, Location, EventTag
+
+#additional serializers
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventTag
+        fields=('tag','tag_screen')
+    tag_screen = serializers.SerializerMethodField('get_screen_tag')
+
+    def get_screen_tag(self,obj):
+        return str(obj)
+
+#serializer for Event class
+class EventSerializer(serializers.ModelSerializer):
+    #location= LocationSerializer()
+    location = serializers.StringRelatedField()
+    tags = TagSerializer(many=True)
+    class Meta:
+        model = Event
+        fields=('title','start_time','end_time','location','description','tags')
--- schedule/views.py
+++ schedule/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .serializers import EventSerializer
+from rest_framework import generics
+from .models import Event
+
+#Optional optimization feature, only use if needed
+#REQUIRES: upper limit timestamp to query results from (optional parameter), direction to query events from
+#if none specified, will either load from first event (if system time is before first event or after last event)
+#or will load first event that hasn't started yet. (if systemtime is during the con)
+#MODIFIES: none
+#EFFECTS: loads up the next or previous 10 (adjustable) events on the event roster, or less, if not enough events to query for
+
+class EventListing(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer 
 
 def index(request):
     return HttpResponse("schedule")
